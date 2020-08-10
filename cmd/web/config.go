a1fbb6db2b93f09cb2524308aec221ff6395eb79
Michael Flaxman <mflaxman@gmail.com>
2020-08-10 12:59:58 -0500

    refactor

--- cmd/web/config.go
+++ cmd/web/config.go
@@ -8,11 +8,10 @@ import (
 
 type Config struct {
 	// TODO: using Prod defaults (except for secure stuff hidden in credentials.yaml)
-	SQLiteFilePath      string
-	SeverHost           string
-	ServerPort          string
-	RSAPrivateKeyString string
-	RSAPrivateKeyBytes  []byte
+	SQLiteFilePath     string
+	SeverHost          string
+	ServerPort         string
+	RSAPrivateKeyBytes []byte
 }
 
 // CFG is an exportable single source of truth for config info & secrets
@@ -29,21 +28,19 @@ func defaultRead(envVar string, defaultVal string) (toSet string) {
 func InitConfig() {
 	log.Println("Generating configs...")
 
-	SQLiteFilePath := defaultRead("SQLITE_FILEPATH", "opsep.db")
-	SeverHost := defaultRead("SERVER_HOST", "localhost")
-	ServerPort := defaultRead("SERVER_PORT", "80")
-	RSAPrivateKey := strings.TrimSpace(os.Getenv("RSA_PRIVATE_KEY"))
+	RSAPrivateKeyString := strings.TrimSpace(os.Getenv("RSA_PRIVATE_KEY"))
+	goodRSAPrefix := strings.HasPrefix(RSAPrivateKeyString, "-----BEGIN RSA PRIVATE KEY-----") == true
+	goodRSASuffix := strings.HasSuffix(RSAPrivateKeyString, "-----END RSA PRIVATE KEY-----") == true
 
-	if strings.HasPrefix(RSAPrivateKey, "-----BEGIN RSA PRIVATE KEY-----") == false || strings.HasSuffix(RSAPrivateKey, "-----END RSA PRIVATE KEY-----") == false {
+	if !goodRSAPrefix || !goodRSASuffix {
 		panic("RSA Private Key Invalid")
 	}
 
 	cfg := Config{
-		SQLiteFilePath:      SQLiteFilePath,
-		SeverHost:           SeverHost,
-		ServerPort:          ServerPort,
-		RSAPrivateKeyString: RSAPrivateKey,
-		RSAPrivateKeyBytes:  []byte(RSAPrivateKey),
+		SQLiteFilePath:     defaultRead("SQLITE_FILEPATH", "opsep.db"),
+		SeverHost:          defaultRead("SERVER_HOST", "localhost"),
+		ServerPort:         defaultRead("SERVER_PORT", "80"),
+		RSAPrivateKeyBytes: []byte(RSAPrivateKeyString),
 	}
 
 	CFG = &cfg
