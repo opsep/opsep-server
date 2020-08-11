d5ee2ce2606ca9b28dbd32066ab796a3ebb02af7
Michael Flaxman <mflaxman@gmail.com>
2020-08-11 13:12:57 -0500

    trigger only on golang file changes

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,3 +1,3 @@
 # install reflex via `$ go get github.com/cespare/reflex`
 
-SERVER_PORT=8080 RSA_PRIVATE_KEY="$(cat insecure_certs/pem.priv)" reflex -s  --inverse-regex='\.sqlite3$' -- go run *.go
+SERVER_PORT=8080 RSA_PRIVATE_KEY="$(cat insecure_certs/pem.priv)" reflex -s  --regex='\.go$' -- go run *.go
